35eeb60d5b0cd6463a5489b4348c10be04985115
Simon Bradley <simonbradley42@gmail.com>
2024-09-23 21:44:29 +1000

    Create ObjectsAndArrays.js

---
+++ ZTM/JavaScript/ObjectsAndArrays.js
@@ -0,0 +1,32 @@
+// Object and Array Exercise
+// Create an object and an array which we will use in our facebook exercise. 
+
+// 1. Create an object that has properties "username" and "password". Fill those values in with strings.
+var user = {
+    username: "hoid",
+    password: "shutupdesign",
+}
+
+// 2. Create an array which contains the object you have made above and name the array "database".
+var database = [
+    {
+        username: "hoid",
+        password: "shutupdesign",
+    }
+]
+
+// 3. Create an array called "newsfeed" which contains 3 objects with properties "username" and "timeline".
+var newsfeed = [
+    {
+        username: "mitch",
+        password: "gotisgreat",
+    },
+    {
+        username: "dani",
+        password: "abc123",
+    },
+    {
+        username: "joe",
+        password: "123456",
+    },
+]
