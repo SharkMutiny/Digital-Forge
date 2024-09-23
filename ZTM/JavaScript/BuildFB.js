f68db9a946db2b3c42b6e41706f13724b52a8f17
Simon Bradley <simonbradley42@gmail.com>
2024-09-23 23:23:30 +1000

    Update BuildFB.js

---
+++ ZTM/JavaScript/BuildFB.js
@@ -0,0 +1,32 @@
+// Exercise - Build a facebook like with login, database and newsfeed
+
+var database = [
+    {
+        username: "shark",
+        password: "123456",
+    }
+];
+
+var newsFeed = [
+    {
+        username: "Ricky-Bobby",
+        timeline: "If you ain't first, your last",
+    },
+    {
+        username: "Arnold",
+        timeline: "Sully? I left him go.",
+    }
+];
+
+var userNamePrompt = prompt("What's your username?");
+var passwordPrompt = prompt("What's your password?");
+
+function signIn(user, pass) {
+    if (user === database[0].username && pass === database[0].password) {
+        console.log(newsFeed)
+    } else {
+        alert("Sorry, wrong username or password!")
+    }
+};
+
+signIn(userNamePrompt, passwordPrompt);
