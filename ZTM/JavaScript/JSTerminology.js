0a896a520593adde7252982a5f4592d723e290b3
Simon Bradley <simonbradley42@gmail.com>
2024-09-23 23:23:33 +1000

    Create JSTerminology.js

---
+++ ZTM/JavaScript/JSTerminology.js
@@ -0,0 +1,36 @@
+// function declaration
+function newFunction() {
+
+}
+
+// function expression
+var newFunction = function() {
+
+};
+
+// expression ;
+3+2;
+var a = 2;
+
+// calling or invoking a function
+alert();
+newFunction(para1, para2);
+
+// assign a variable
+var a = true;
+
+// function vs method
+function thisIsAFunction() {
+
+}
+
+var obj = {
+    thisIsAMethod: function() {
+
+    }
+}
+
+// access function vs method
+thisIsAFunction()
+
+obj.thisIsAMethod()
