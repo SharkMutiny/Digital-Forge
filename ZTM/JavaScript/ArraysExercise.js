d510d3fc61a93355cfdc79d8dd1e11bd2de0e33e
Simon Bradley <simonbradley42@gmail.com>
2024-09-23 19:52:42 +1000

    Create ArraysExercise.js

---
+++ ZTM/JavaScript/ArraysExercise.js
@@ -0,0 +1,29 @@
+// excercise of modifying arrays
+// using this array
+var array = ["Banana", "Apples", "Oranges", "Blueberries"];
+
+// 1. Remove the Banana from the array
+array.shift();
+array;
+
+// 2. Sort the array in order.
+array.sort();
+array;
+
+// 3. Put "Kiwi" at the end of the array.
+array.push("Kiwi");
+array;
+
+// 4. Remove "Apples" from the array.
+array.splice(0,1);
+array
+
+// 5. Sort the array in reverse order. (Not alphabetical, but reverse
+// the current Array i.e. ['a', 'c', 'b'] becomes ['b', 'c', 'a'])
+array.reverse()
+array
+
+// access "Oranges".
+// using this array,
+var array2 = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+array2[1][1][0]
