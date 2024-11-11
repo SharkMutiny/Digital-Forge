f1dd19e902323391a726bbccdbbe0c2c397f0e1b
Simon Bradley <simonbradley42@gmail.com>
2024-11-11 18:02:02 +1100

    Update Calorie Counter.js

--- freeCodeCamp/JavaScript/Calorie Counter/Calorie Counter.js
+++ freeCodeCamp/JavaScript/Calorie Counter/Calorie Counter.js
@@ -5,3 +5,12 @@ const addEntryButton = document.getElementById('add-entry');
 const clearButton = document.getElementById('clear');
 const output = document.getElementById('output');
 let isError = false;
+
+function cleanInputString(str) {
+    const regex = /[+-\s]/g;
+    return str.replace(regex, '');
+  }
+
+  function isInvalidInput(str) {
+    const regex = /e/;
+  }
