a60b2aec671a1f81d1b097cf5af0b042b9fc6337
Simon Bradley <simonbradley42@gmail.com>
2024-12-05 22:04:47 +1100

    advent  day 1

---
+++ Misc/AdventOfCode2024/Day 1/Day1.1.js
@@ -0,0 +1,12 @@
+// Advent of Code 2024 Day 1.1
+
+// import or read the inupt files
+// read column 1
+// read column 2
+// find the lowest value in each column
+// compare the lowest values
+// log the difference
+// remove those values from the array
+// repeat until the array is empty
+// log the sum of the differences
+
