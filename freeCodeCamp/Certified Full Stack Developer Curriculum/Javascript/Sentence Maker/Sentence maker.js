bde7fb8a9037153a7b621f1693377f1ecbbcc5f8
Simon Bradley <simonbradley42@gmail.com>
2025-06-27 16:21:59 +1000

    Create Sentence maker.js
    completed lesson

---
+++ freeCodeCamp/Certified Full Stack Developer Curriculum/Javascript/Sentence Maker/Sentence maker.js
@@ -0,0 +1,21 @@
+let adjective = "delightful";
+let noun = "dragon";
+let verb = "think";
+let place = "palace";
+let adjective2 = "fancy";
+let noun2 = "human";
+
+let firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun +  " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("First story: " + firstStory);
+
+adjective = "funny";
+noun = "cat";
+verb = "think";
+place = "forest";
+adjective2 = "pretty";
+noun2 = "bird";
+
+let secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun +  " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("Second story: " + secondStory)
