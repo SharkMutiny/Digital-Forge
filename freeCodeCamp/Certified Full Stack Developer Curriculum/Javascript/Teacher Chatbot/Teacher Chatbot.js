b2f6eff54ecf004a7c4cfe8fcf78d3fb40b4cb70
Simon Bradley <simonbradley42@gmail.com>
2025-07-03 21:03:11 +1000

    Create Teacher Chatbot.js
    working on lesson

---
+++ freeCodeCamp/Certified Full Stack Developer Curriculum/Javascript/Teacher Chatbot/Teacher Chatbot.js
@@ -0,0 +1,16 @@
+console.log("Hi there!");
+
+const botName = "teacherBot";
+
+const greeting = `My name is ${botName}.`;
+console.log(greeting);
+
+const subject = "JavaScript";
+const topic = "strings";
+
+const sentence = `Today, you will learn about ${topic} in ${subject}.`;
+console.log(sentence);
+
+const strLengthIntro = `Here is an example of using the length property on the word ${subject}.`;
+console.log(strLengthIntro);
+console.log(subject.length);
