9763c3325f607b94fca4bd637c78a0b62fc88279
Simon Bradley <simonbradley42@gmail.com>
2025-09-17 20:46:13 +1000

    Create Record Collection.js
    completed FCC JS Objects Lab

---
+++ freeCodeCamp/Certified Full Stack Developer Curriculum/Javascript/Record Collection/Record Collection.js
@@ -0,0 +1,46 @@
+const recordCollection = {
+  2548: {
+    albumTitle: 'Slippery When Wet',
+    artist: 'Bon Jovi',
+    tracks: ['Let It Rock', 'You Give Love a Bad Name']
+  },
+  2468: {
+    albumTitle: '1999',
+    artist: 'Prince',
+    tracks: ['1999', 'Little Red Corvette']
+  },
+  1245: {
+    artist: 'Robert Palmer',
+    tracks: []
+  },
+  5439: {
+    albumTitle: 'ABBA Gold'
+  }
+};
+
+
+function updateRecords(records, id, prop, value) {
+  if (value === "") {
+    delete records[id][prop]
+    return records
+  } else if (prop !== "tracks" && value !== "") {
+    records[id][prop] = value
+    return records
+  } else if (prop === "tracks" && value !== "" && records[id][prop] === undefined) {
+    records[id][prop] = []
+    records[id][prop].unshift(value)
+    return records
+  }else if (prop === "tracks" && value !== "" && records[id][prop] !== undefined) {
+    records[id][prop].push(value)
+    return records
+  }
+  return records
+}
+
+// console.log(updateRecords(recordCollection, 5439, "artist", "ABBA"))
+// console.log(updateRecords(recordCollection, 5439, "tracks", "Take a Chance on Me"))
+// console.log(updateRecords(recordCollection, 2548, "artist", ""))
+// console.log(updateRecords(recordCollection, 1245, "tracks", "Addicted to Love"))
+// console.log(updateRecords(recordCollection, 2468, "tracks", "Free"))
+// console.log(updateRecords(recordCollection, 2548, "tracks", ""))
+// console.log(updateRecords(recordCollection, 1245, "albumTitle", "Riptide"))
