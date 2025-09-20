8a2850823be47e71f3cd15aaa7ef7fb31645044a
Simon Bradley <simonbradley42@gmail.com>
2025-09-20 11:27:43 +1000

    changing file tree

--- ZTM/JavaScript/DOMEvents.js
+++ ZTM/JavaScript/DOMEvents.js
@@ -1,7 +1,7 @@
 // DOM Events exercise - Making a Shopping List App
 /* var button = document.getElementsByTagName("button")[0]
 
-button.addEventListerner("click", function() {
+button.addEventListener("click", function() {
     console.log("CLICK!!!");
 }) */
 
@@ -10,26 +10,26 @@ var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
 
 function inputLength() {
-  return input.value.length;
+	return input.value.length;
 }
 
 function createListElement() {
-  var li = document.createElement("li");
-  li.appendChild(document.createTextNode(input.value));
-  ul.appendChild(li);
-  input.value = "";
+	var li = document.createElement("li");
+	li.appendChild(document.createTextNode(input.value));
+	ul.appendChild(li);
+	input.value = "";
 }
 
 function addListAfterClick() {
-  if (inputLength() > 0) {
-    createListElement();
-  }
+	if (inputLength() > 0) {
+		createListElement();
+	}
 }
 
 function addListAfterKeypress(event) {
-  if (inputLength() > 0 && event.keyCode === 13) {
-    createListElement();
-  }
+	if (inputLength() > 0 && event.keyCode === 13) {
+		createListElement();
+	}
 }
 
 button.addEventListener("click", addListAfterClick);
