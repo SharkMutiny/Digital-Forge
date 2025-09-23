24b9afc678140f83ee8067978dd3163e16b1cbe6
Simon Bradley <simonbradley42@gmail.com>
2025-09-23 20:17:31 +1000

    Create ES5 and ES6.js
    completed ZTM exercise

---
+++ ZTM/JavaScript/ES5 and ES6/ES5 and ES6.js
@@ -0,0 +1,83 @@
+// change everything below to the newer Javascript!
+
+
+// let + const
+// var a = 'test';
+// var b = true;
+// var c = 789;
+// a = 'test2';
+
+// Using let and const
+let a = 'test';
+const b = true;
+const c = 789;
+a = 'test2';
+
+
+// Destructuring
+var person = {
+    firstName : "John",
+    lastName  : "Doe",
+    age       : 50,
+    eyeColor  : "blue"
+};
+var firstName = person.firstName;
+var lastName = person.lastName;
+var age = person.age;
+var eyeColor = person.eyeColor;
+// Using destructuring
+let { firstName, lastName, age, eyeColor } = person;
+
+
+// Object properties
+var d = 'test';
+var e = true;
+var f = 789;
+
+var okObj = {
+  d: d,
+  e: e,
+  f: f
+};
+// Using object property shorthand
+let okObj = { d, e, f };
+
+
+// Template strings
+var message = "Hello " + firstName + " have I met you before? I think we met in " + city + " last summer no???";
+// Using template literals
+let newMessage = `Hello ${firstName} have I met you before? I think we met in ${city} last summer no???`;
+
+
+// default arguments
+// default age to 10;
+function isValidAge(age) {
+    return age
+}
+// Using default arguments
+function isValidAge(age = 10) {
+    return age;
+}
+
+
+// Symbol
+// Create a symbol: "This is my first Symbol"
+let mySymbol = Symbol("This is my first Symbol");
+
+
+// Arrow functions
+function whereAmI(username, location) {
+    if (username && location) {
+        return "I am not lost";
+    } else {
+        return "I am totally lost!";
+    }
+}
+// Using arrow functions
+const whereAmI = (username, location) => {
+    if (username && location) {
+        return "I am not lost";
+    } else {
+        return "I am totally lost!";
+    }
+}
