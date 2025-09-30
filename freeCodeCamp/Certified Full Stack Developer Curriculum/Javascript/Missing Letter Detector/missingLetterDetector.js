0ceda09eb0a84280bd5ee8bcc211099a31b8eb7d
Simon Bradley <simonbradley42@gmail.com>
2025-09-30 14:31:37 +1000

    Create missingLetterDetector.js
    start of FCC Loops Lab

---
+++ freeCodeCamp/Certified Full Stack Developer Curriculum/Javascript/Missing Letter Detector/missingLetterDetector.js
@@ -0,0 +1,10 @@
+function fearNotLetter(str) {
+let alpha = "abcdefghijklmnopqrstuvwxyz"
+  if (alpha.includes(str)) {
+    return undefined
+  } else {
+    return "haazaa"  // replace with actual logic
+  }
+}
+
+console.log(fearNotLetter("abcdf"))
