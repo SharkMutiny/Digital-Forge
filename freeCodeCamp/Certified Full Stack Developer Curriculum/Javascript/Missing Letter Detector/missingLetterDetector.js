d69408e045798b248fe79d98675cc3057972894a
Simon Bradley <simonbradley42@gmail.com>
2025-10-01 22:03:21 +1000

    Update missingLetterDetector.js
    completed FCC Loops Lab
    did a second version that would return every missing letter

--- freeCodeCamp/Certified Full Stack Developer Curriculum/Javascript/Missing Letter Detector/missingLetterDetector.js
+++ freeCodeCamp/Certified Full Stack Developer Curriculum/Javascript/Missing Letter Detector/missingLetterDetector.js
@@ -1,10 +1,39 @@
 function fearNotLetter(str) {
-let alpha = "abcdefghijklmnopqrstuvwxyz"
-  if (alpha.includes(str)) {
+const alphabet = "abcdefghijklmnopqrstuvwxyz"
+const startIndex = alphabet.indexOf(str[0])
+const endIndex = alphabet.indexOf(str[str.length - 1])
+const subAlphabet = alphabet.slice(startIndex, endIndex + 1)
+
+  for (let i = 0; i < subAlphabet.length; i++) {
+    if (str.indexOf(subAlphabet[i]) === -1) {
+      return subAlphabet[i]
+    }
+  }
+  return undefined
+}
+
+console.log(fearNotLetter("abce"))
+
+
+
+// Alternative solution that returns all missing letters
+function fearNotLetter(str) {
+const alphabet = "abcdefghijklmnopqrstuvwxyz"
+const startIndex = alphabet.indexOf(str[0])
+const endIndex = alphabet.indexOf(str[str.length - 1])
+const subAlphabet = alphabet.slice(startIndex, endIndex + 1)
+let missing = ''
+
+  if (alphabet.includes(str)) {
     return undefined
-  } else {
-    return "haazaa"  // replace with actual logic
+    } else {
+      for (let i = 0; i < subAlphabet.length; i++) {
+        if (str.indexOf(subAlphabet[i]) === -1) {
+        missing += subAlphabet[i]
+        } 
+      }  
+    return missing
   }
 }
 
-console.log(fearNotLetter("abcdf"))
+console.log(fearNotLetter("abdfhlpqtxyz"))
