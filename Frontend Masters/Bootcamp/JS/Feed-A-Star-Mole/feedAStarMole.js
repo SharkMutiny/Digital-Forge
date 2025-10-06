de19916c03f5d8033701e36b24a3f52d28fa2b2a
Simon Bradley <simonbradley42@gmail.com>
2025-10-06 15:35:08 +1100

    finished project
    completed FEM Bootcamp Project

--- Frontend Masters/Bootcamp/JS/Feed-A-Star-Mole/feedAStarMole.js
+++ Frontend Masters/Bootcamp/JS/Feed-A-Star-Mole/feedAStarMole.js
@@ -1,5 +1,20 @@
+let score = 0
+const MAX_SCORE = 10;
+
 function getSadInterval() {
     return Date.now() + 1000;
+}
+
+function getGoneInterval() {
+    return Date.now() + Math.floor(Math.random() * 18000) + 2000;
+}
+
+function getHungryInterval() {
+    return Date.now() + Math.floor(Math.random() * 3000) + 2000;
+}
+
+function getKingStatus() {
+    return Math.random() > 0.9;
 }
 
 const moles = [
@@ -65,3 +80,95 @@ const moles = [
     },
 ];
 
+function getNextStatus(mole) {
+    switch(mole.status) {
+        case 'sad':
+        case 'fed':
+            mole.status = 'leaving';
+            mole.next = getSadInterval();
+                if (mole.king) {
+                    mole.node.children[0].src = './images/king-mole-leaving.png';
+                } else {
+                    mole.node.children[0].src = './images/mole-leaving.png';
+                };
+            break;
+        case 'leaving':
+            mole.status = 'gone';
+            mole.next = getGoneInterval();
+            mole.node.children[0].classList.add('gone');
+            break;
+        case 'gone':
+            mole.status = 'hungry';
+            mole.king = getKingStatus();
+            mole.next = getHungryInterval();
+            mole.node.children[0].classList.add('hungry');
+            mole.node.children[0].classList.remove('gone');
+                if (mole.king) {
+                    mole.node.children[0].src = './images/king-mole-hungry.png';
+                } else {
+                    mole.node.children[0].src = './images/mole-hungry.png';
+                };
+            break;
+        case 'hungry':
+            mole.status = 'sad';
+            mole.next = getSadInterval();
+            mole.node.children[0].classList.remove('hungry');
+                if (mole.king) {
+                    mole.node.children[0].src = './images/king-mole-sad.png';
+                } else {
+                    mole.node.children[0].src = './images/mole-sad.png';
+                };
+            break;
+    }
+}
+
+function feed(e) {
+    if (!e.target.classList.contains('hungry')) {
+        return;
+    }
+    
+    const mole = moles[parseInt(e.target.dataset.index)];
+
+    mole.status = 'fed';
+    mole.next = getSadInterval();
+    mole.node.children[0].classList.remove('hungry');
+        if (mole.king) {
+            score += 4;
+            mole.node.children[0].src = "./images/king-mole-fed.png";
+        } else {
+            score++;
+            mole.node.children[0].src = "./images/mole-fed.png";
+        }
+
+    if (score >= MAX_SCORE) {
+        win();
+    }
+
+    document.querySelector('.worm-container').style.width = `${(score / MAX_SCORE) * 100}%`;
+
+}
+
+function win() {
+    document.querySelector('.bg').classList.add('hide');
+    document.querySelector('.win').classList.add('show');
+}
+
+let runAgainAt = Date.now() + 100;
+
+function nextFrame() {
+    const now = Date.now();
+
+    if (runAgainAt <= now) {
+        for(let i = 0; i < moles.length; i++) {
+            if (moles[i].next <= now) {
+                getNextStatus(moles[i]);
+            }
+        }
+        runAgainAt = now + 100;
+    }
+    requestAnimationFrame(nextFrame);
+}
+
+document.querySelector('.bg').addEventListener('click', feed)
+
+nextFrame();
