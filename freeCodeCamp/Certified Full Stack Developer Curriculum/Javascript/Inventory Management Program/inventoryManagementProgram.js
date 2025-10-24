f98118aea58df454f61dc865492796fb6f6c2cbb
SharkMutiny <simonbradley42@gmail.com>
2025-10-24 15:32:00 +1100

    Update inventoryManagementProgram.js
    Completed FCC JS Lab

--- freeCodeCamp/Certified Full Stack Developer Curriculum/Javascript/Inventory Management Program/inventoryManagementProgram.js
+++ freeCodeCamp/Certified Full Stack Developer Curriculum/Javascript/Inventory Management Program/inventoryManagementProgram.js
@@ -1,35 +1,42 @@
-let inventory = [];
+let inventory = []
 
 function findProductIndex(productName) {
-  const lowerCaseName = productName.toLowerCase();
-  return inventory.findIndex(product => product.name === lowerCaseName);
+  const lowerCaseName = productName.toLowerCase()
+  return inventory.findIndex(product => product.name === lowerCaseName)
 }
+
 function addProduct(product) {
-  const index = findProductIndex(product.name);
-    if (index !== -1) {
-    inventory[index].quantity += product.quantity;
-    console.log(`${product.name} quantity updated`);
+  const index = findProductIndex(product.name)
+  if (index !== -1) {
+    inventory[index].quantity += product.quantity
+    console.log(`${product.name.toLowerCase()} quantity updated`)
   } else {
-    inventory.push({ name: product.name.toLowerCase(), quantity: product.quantity });
-    console.log(`${product.name} added to inventory`);
+    inventory.push({ name: product.name.toLowerCase(), quantity: product.quantity })
+    console.log(`${product.name.toLowerCase()} added to inventory`)
   }
 }
 
 function removeProduct(productName, quantity) {
-  const index = findProductIndex(productName);
-  if (index === -1) {
-    console.log(`${productName} not found`);
-    return;
-  }
-    const product = inventory[index];
+  const index = findProductIndex(productName)
+    if (index === -1) {
+      console.log(`${productName.toLowerCase()} not found`)
+      return
+    }
+
+  const product = inventory[index]
     if (product.quantity < quantity) {
-    console.log(`Not enough ${productName} available, remaining pieces: ${product.quantity}`);
-    return;
+      console.log(`Not enough ${productName.toLowerCase()} available, remaining pieces: ${product.quantity}`)
+      return
     }
-    product.quantity -= quantity;
-    if (product.quantity === 0) {
-    inventory.splice(index, 1);
-    } else {
-    console.log(`Remaining ${productName} pieces: ${product.quantity}`);
-    }
+  product.quantity -= quantity
+  if (product.quantity === 0) {
+    inventory.splice(index, 1)
+  } else {
+  console.log(`Remaining ${productName.toLowerCase()} pieces: ${product.quantity}`)
+  }
 }
+
+
+// console.log(addProduct({name: "FLOUR", quantity: 5}))
+// console.log(inventory)
+// console.log(removeProduct("FLOUR", 7))
