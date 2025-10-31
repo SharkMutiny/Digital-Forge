d43b59caa6b66bc92b5a7d912865019552baac7a
SharkMutiny <simonbradley42@gmail.com>
2025-10-31 12:25:36 +1100

    initial commit
    initial commit.
    commented description of challenge and initial starting point so I can work on challenge offline

---
+++ freeCodeCamp/Daily Coding Challenge/August/2025-08-21.js
@@ -0,0 +1,10 @@
+// Mile Pace
+// Given a number of miles ran, and a time in "MM:SS" (minutes:seconds) it took to run those miles, return a string for the average time it took to run each mile in the format "MM:SS".
+
+// Add leading zeros when needed.
+
+
+function milePace(miles, duration) {
+
+  return miles;
+}
---
+++ freeCodeCamp/Daily Coding Challenge/August/2025-08-27.js
@@ -0,0 +1,13 @@
+
+// Unorder of Operations
+// Given an array of integers and an array of string operators, apply the operations to the numbers sequentially from left-to-right. Repeat the operations as needed until all numbers are used. Return the final result.
+
+// For example, given [1, 2, 3, 4, 5] and ['+', '*'], return the result of evaluating 1 + 2 * 3 + 4 * 5 from left-to-right ignoring standard order of operations.
+
+// Valid operators are +, -, *, /, and %.
+
+
+function evaluate(numbers, operators) {
+
+  return numbers;
+}
