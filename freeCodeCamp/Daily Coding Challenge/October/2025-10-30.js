925ba2661c868b1e58cc8d337e2a77d001c9078a
SharkMutiny <simonbradley42@gmail.com>
2025-10-31 20:53:31 +1100

    initial commit
    initial commit.
    commented description of challenge and initial starting point so I can work on challenge offline

---
+++ freeCodeCamp/Daily Coding Challenge/October/2025-10-30.js
@@ -0,0 +1,10 @@
+// Nth Prime
+// A prime number is a positive integer greater than 1 that is divisible only by 1 and itself. The first five prime numbers are 2, 3, 5, 7, and 11.
+
+// Given a positive integer n, return the nth prime number. For example, given 5 return the 5th prime number: 11.
+
+
+function nthPrime(n) {
+
+  return n;
+}
