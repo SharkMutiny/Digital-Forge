873a732b1902085f3c09fa7eb4fc08e4f8665a93
SharkMutiny <simonbradley42@gmail.com>
2025-10-31 22:41:20 +1100

    Update 2025-08-19.js
    completed FCC Daily Challenge

--- freeCodeCamp/Daily Coding Challenge/August/2025-08-19.js
+++ freeCodeCamp/Daily Coding Challenge/August/2025-08-19.js
@@ -1,7 +1,16 @@
 // Sum of Squares
 // Given a positive integer up to 1,000, return the sum of all the integers squared from 1 up to the number.
 
+
 function sumOfSquares(n) {
+  // Validate input
+  if (n < 1 || n > 1000) {
+    return "Input must be a positive integer up to 1,000.";
+  }
+  // Using the formula n(n + 1)(2n + 1) / 6 to calculate the sum of squares
+  n = n * (n + 1) * (2 * n + 1) / 6;
 
   return n;
 }
+
+// console.log(sumOfSquares(5))
