19a7e00a0196e1aee0c80268dd4a6397293f5cdb
SharkMutiny <simonbradley42@gmail.com>
2025-11-13 18:02:34 +1100

    Update 2025-08-20.js
    added title

--- freeCodeCamp/Daily Coding Challenge/August/2025-08-20.js
+++ freeCodeCamp/Daily Coding Challenge/August/2025-08-20.js
@@ -1,3 +1,4 @@
+// Daily Coding Challenge: August 20, 2025
 // 3 Strikes
 // Given an integer between 1 and 10,000, return a count of how many numbers from 1 up to that integer whose square contains at least one digit 3.
 
