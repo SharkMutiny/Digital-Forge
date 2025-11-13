c306182175861b8089e270c99833cff306552106
SharkMutiny <simonbradley42@gmail.com>
2025-11-13 18:02:20 +1100

    Update 2025-08-17.js
    added title

--- freeCodeCamp/Daily Coding Challenge/August/2025-08-17.js
+++ freeCodeCamp/Daily Coding Challenge/August/2025-08-17.js
@@ -1,3 +1,4 @@
+// Daily Coding Challenge: August 17, 2025
 // Targeted Sum
 // Given an array of numbers and an integer target, find two unique numbers in the array that add up to the target value. Return an array with the indices of those two numbers, or "Target not found" if no two numbers sum up to the target.
 
