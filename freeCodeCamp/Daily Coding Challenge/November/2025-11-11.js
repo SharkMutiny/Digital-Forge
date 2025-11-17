c9166fd121f030eeae65b443e190b66c78f62125
SharkMutiny <simonbradley42@gmail.com>
2025-11-17 19:57:34 +1100

    Add skeleton for vowels and consonants counter
    Created initial file for the November 11, 2025 daily coding challenge. Added problem description and function stub for counting vowels and consonants in a string.

---
+++ freeCodeCamp/Daily Coding Challenge/November/2025-11-11.js
@@ -0,0 +1,13 @@
+// Daily Coding Challenge: November 11, 2025
+// Vowels and Consonants
+// Given a string, return an array with the number of vowels and number of consonants in the string.
+
+// Vowels consist of a, e, i, o, u in any case.
+// Consonants consist of all other letters in any case.
+// Ignore any non-letter characters.
+// For example, given "Hello World", return [3, 7].
+
+function count(str) {
+
+  return str;
+}
