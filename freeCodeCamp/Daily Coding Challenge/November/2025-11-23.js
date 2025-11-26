7c717c7bbc8ecffbef8da001ec6f50259dc16664
SharkMutiny <simonbradley42@gmail.com>
2025-11-26 18:59:14 +1100

    Add Daily Coding Challenge stubs for Nov 18-26, 2025
    Added initial JavaScript files for the Daily Coding Challenge for November 18 through November 26, 2025. Each file contains the problem description and a stub function for implementation.

---
+++ freeCodeCamp/Daily Coding Challenge/November/2025-11-23.js
@@ -0,0 +1,14 @@
+// Daily Coding Challenge: November 23, 2025
+// Character Count
+// Given a sentence string, return an array with a count of each character in alphabetical order.
+
+// Treat upper and lowercase letters as the same letter when counting.
+// Ignore numbers, spaces, punctuation, etc.
+// Return the count and letter in the format "letter count". For instance, "a 3".
+// All returned letters should be lowercase.
+// Do not return a count of letters that are not in the given string.
+
+function countCharacters(sentence) {
+
+  return sentence;
+}
---
+++ freeCodeCamp/Daily Coding Challenge/November/2025-11-25.js
@@ -0,0 +1,12 @@
+// Daily Coding Challenge: November 25, 2025
+// FizzBuzz
+// Given an integer (n), return an array of integers from 1 to n (inclusive), replacing numbers that are multiple of:
+
+// 3 with "Fizz".
+// 5 with "Buzz".
+// 3 and 5 with "FizzBuzz".
+
+function fizzBuzz(n) {
+
+  return n;
+}
